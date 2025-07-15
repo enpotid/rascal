f9b2eb0349335bd70633aef02c61c0541aebcbdd
enpotid <xian101221@gmail.com>
2025-07-15 22:35:57 +0900

    text input

--- src/main.rs
+++ src/main.rs
@@ -20,12 +20,11 @@ fn main() -> io::Result<()> {
 
 #[derive(Debug, Default)]
 pub struct App {
-    counter: u8,
     exit: bool,
+    text: String,
 }
 
 impl App {
-    /// runs the application's main loop until the user quits
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
         while !self.exit {
             terminal.draw(|frame| self.draw(frame))?;
@@ -40,8 +39,6 @@ impl App {
 
     fn handle_events(&mut self) -> io::Result<()> {
         match event::read()? {
-            // it's important to check that the event is a key press event as
-            // crossterm also emits key release and repeat events on Windows.
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 self.handle_key_event(key_event)
             }
@@ -52,9 +49,11 @@ impl App {
 
     fn handle_key_event(&mut self, key_event: KeyEvent) {
         match key_event.code {
-            KeyCode::Char('q') => self.exit(),
-            KeyCode::Left => self.decrement_counter(),
-            KeyCode::Right => self.increment_counter(),
+            KeyCode::Char(c) => self.text.push(c),
+            KeyCode::Esc => self.exit(),
+            KeyCode::Backspace => {
+                let _ = self.text.pop();
+            }
             _ => {}
         }
     }
@@ -62,39 +61,21 @@ impl App {
     fn exit(&mut self) {
         self.exit = true;
     }
-
-    fn increment_counter(&mut self) {
-        self.counter += 1;
-    }
-
-    fn decrement_counter(&mut self) {
-        self.counter -= 1;
-    }
 }
 
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Counter App Tutorial ".bold());
-        let instructions = Line::from(vec![
-            " Decrement ".into(),
-            "<Left>".blue().bold(),
-            " Increment ".into(),
-            "<Right>".blue().bold(),
-            " Quit ".into(),
-            "<Q> ".blue().bold(),
-        ]);
+        let title = Line::from(" Rascal ".bold());
+        let instructions = Line::from(vec![" Quit ".into(), "<Esc> ".blue().bold()]);
         let block = Block::bordered()
             .title(title.centered())
             .title_bottom(instructions.centered())
             .border_set(border::THICK);
 
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Value: ".into(),
-            self.counter.to_string().yellow(),
-        ])]);
+        let counter_text = Text::from(vec![Line::from(vec![self.text.clone().into()])]);
 
         Paragraph::new(counter_text)
-            .centered()
+            .left_aligned()
             .block(block)
             .render(area, buf);
     }
